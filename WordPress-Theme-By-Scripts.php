c1bb4f0e54e4fa0558ac8621562fa99b33c7554f
Jimmy B <chips_xp@yahoo.com>
2024-02-18 18:39:51 -0500

    Initial commit

---
+++ WordPress-Theme-By-Scripts.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: WordPress Theme from scripts
+ * Description: WordPress Theme with plugins
+ * Version: 1.0
+ * Author: Firstname Lastname
+ * License: GPLv2 or later
+ * Text Domain: wordpress-scripts-plugin
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
